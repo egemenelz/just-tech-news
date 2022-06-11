8f7ea89b864931867961b2eb362b6a0e8c550e70
egemenelz <egemenelz@gmail.com>
2022-06-10 22:47:27 -0400

    LOGIN

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -26,7 +26,13 @@ router.get('/:id', (req, res) => {
                 res.status(404).json({ message: 'No user found with this id' });
                 return;
             }
-            res.json(dbUserData);
+            const validPassword = dbUserData.checkPassword(req.body.password);
+            if (!validPassword) {
+                res.status(400).json({ message: 'Incorrect password!' });
+                return;
+            }
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' })
         })
         .catch(err => {
             console.log(err);
@@ -49,13 +55,33 @@ router.post('/', (req, res) => {
         });
 });
 
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
 
-// PUT /api/users/1
+        res.json({ user: dbUserData });
+
+        // Verify user
+
+    });
+});
+
+
+
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
-    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+    // pass in req.body instead to only update what's passed through
     User.update(req.body, {
+        individualHooks: true,
         where: {
             id: req.params.id
         }
